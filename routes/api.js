22fdf49cd3d1f5633d7519ba704e495175e8233f
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2020-01-12 10:12:08 +0200

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,77 @@ const router = express.Router();
 
 const Property = require("../models/Property");
 
+const User = require("../models/User");
+
+const Agent = require("../models/Agent");
+
+const client = require("../index");
+
+router.get("/userRegistration",function(req,res){
+  res.send("You are about to register, user!");
+});
+
+router.get("/AgentRegistration",function(req,res){
+  res.send("You are about to register, Agent!");
+});
 
 
+router.post("/getProperty",function(req,res){
+  res.send("requested properties");
+
+});
+
+router.post("/userRegistration",function(req,res){
+  User.create(req.body).then(function(user){
+    console.log(user);
+    res.send(user);
+
+  });
+});
+
+router.post("/userLogin",function(req,res){
+  User.findOne({email:req.body.email}).then(function(user){    // returns updated ninja
+    if(user){
+      if (user.password===req.body.password){
+        res.send(user);
+      }
+      else{
+        res.send("Incorrect password!");
+      }
+
+    }
+    else{
+      res.send("You are not registered!");
+    }
+
+  });
+});
 
 
+router.post("/AgentRegistration",function(req,res){
+  Agent.create(req.body).then(function(agent){
+    console.log(agent);
+    res.send(agent);
 
-router.get("/getProperty",function(req,res){
-  res.send("get property");
+  });
+});
+
+router.post("/agentLogin",function(req,res){
+  Agent.findOne({email:req.body.email}).then(function(agent){    // returns updated ninja
+    if(agent){
+      if (agent.password===req.body.password){
+        res.send(agent);
+      }
+      else{
+        res.send("Incorrect password!");
+      }
+
+    }
+    else{
+      res.send("Agent not registered!");
+    }
+
+  });
 });
 
 router.post("/addProperty",function(req,res,next){
