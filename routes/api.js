7b6050c4c454fdcfcd5f012dee5bc2ac4cf79c24
NkanyisoSigwaza <38808434+NkanyisoSigwaza@users.noreply.github.com>
2020-01-16 10:28:09 +0200

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -1,114 +1,27 @@
 const express = require("express");
 
-const router = express.Router();
+const app = express();
 
-const Property = require("../models/Property");
+const bodyParser = require("body-parser");
 
-const User = require("../models/User");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const Agent = require("../models/Agent");
+app.set("view engine","ejs");
 
-const client = require("../index");
 
-router.get("/userRegistration",function(req,res){
-  res.send("You are about to register, user!");
+app.get("/signup",function(req,res){
+  res.render("signup");
 });
 
-router.get("/AgentRegistration",function(req,res){
-  res.send("You are about to register, Agent!");
+app.post("/eg",urlencodedParser,function(req,res){
+  console.log(req.body);
+  res.send(req.body);
 });
 
-
-router.post("/getProperty",function(req,res){
-  res.send("requested properties");
-
-});
-
-router.post("/userRegistration",function(req,res){
-  User.create(req.body).then(function(user){
-    console.log(user);
-    res.send(user);
-
-  });
-});
-
-router.post("/userLogin",function(req,res){
-  User.findOne({email:req.body.email}).then(function(user){    // returns updated ninja
-    if(user){
-      if (user.password===req.body.password){
-        res.send(user);
-      }
-      else{
-        res.send("Incorrect password!");
-      }
-
-    }
-    else{
-      res.send("You are not registered!");
-    }
-
-  });
-});
-
-
-router.post("/AgentRegistration",function(req,res){
-  Agent.create(req.body).then(function(agent){
-    console.log(agent);
-    res.send(agent);
-
-  });
-});
-
-router.post("/agentLogin",function(req,res){
-  Agent.findOne({email:req.body.email}).then(function(agent){    // returns updated ninja
-    if(agent){
-      if (agent.password===req.body.password){
-        res.send(agent);
-      }
-      else{
-        res.send("Incorrect password!");
-      }
-
-    }
-    else{
-      res.send("Agent not registered!");
-    }
-
-  });
-});
-
-router.post("/addProperty",function(req,res,next){
-  //res.send(req.body);
-  Property.create(req.body).then(function(property){
-
-    res.send(property);  // once new ninja has been succesfully added to the database
-
-  }).catch(next);   // for error handeling if ninja.create fails program will jump to next midle weae
+/*router.get("/eg",function(req,res){
+  res.send("get req");
+});*/
 
 
 
-});
-
-router.put("/updateProperty/:id",function(req,res){
-
-  Property.findByIdAndUpdate({_id:req.params.id},req.body).then(function(property){
-
-    Property.findOne({_id:req.params.id}).then(function(property){    // returns updated property
-
-      res.send(property);
-})
-});
-
-
-});
-
-router.delete("/deleteProperty/:id",function(req,res){
-
-  Property.findByIdAndRemove({_id:req.params.id}).then(function(property){
-
-    res.send(property); //pass id of object you want to delete at end of url
-});
-
-});
-
-module.exports = router;
+module.exports = app;
